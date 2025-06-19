74b81a8ee73bb614aa4d732ec26f15ae2963de37
Maryam-mj <mariamaliabdi12@gmail.com>
2025-06-19 15:04:01 -0700

    add

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
-# your code goes here!
+
+class Anagram:
+
+    def __init__(self, word):
+        self.word = word.lower()
+        self.sorted_word = sorted(self.word)
+
+    def match(self, candidates):
+        return [
+            candidate for candidate in candidates
+            if candidate.lower() != self.word and sorted(candidate.lower()) == self.sorted_word
+        ]
